b286fa1c01b85cb1c3b29c5bf1b50a83aad9eac5
46319943 <495384481@qq.com>
2024-03-13 10:10:44 +0800

    Fix the style for new version of matplotlib

--- georegression/visualize/__init__.py
+++ georegression/visualize/__init__.py
@@ -8,7 +8,8 @@ default_folder.mkdir(exist_ok=True)
 # Set default style for matplotlib
 
 # Control style to make it consistent with the Plotly
-plt.style.use('seaborn')
+# plt.style.use('seaborn')
+plt.style.use('seaborn-v0_8')
 # Font family
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams["font.size"] = 12
