fdae87161c656e28bdde18592f1ef62ce1110d9e
46319943 <495384481@qq.com>
2024-03-13 23:55:24 +0800

    Finish the preliminary result for the simulation fitting.

--- georegression/simulation/simulation_for_fitting.py
+++ georegression/simulation/simulation_for_fitting.py
@@ -128,6 +128,30 @@ def coef_auto_gau_weak():
     return coef_sum
 
 
+def coef_auto_gau_strong():
+    coef_radial = radial_coefficient(np.array([0, 0]), 1 / np.sqrt(200))
+    coef_dir = directional_coefficient(np.array([1, 1]))
+
+    gau_coef_list = []
+    for _ in range(1000):
+        # Randomly generate the parameters for gaussian coefficient
+        center = np.random.uniform(-10, 10, 2)
+        amplitude = np.random.uniform(1, 2)
+        sign = np.random.choice([-1, 1])
+        amplitude *= sign
+        sigma1 = np.random.uniform(0.2, 1)
+        sigma2 = np.random.uniform(0.2, 1)
+        cov = np.random.uniform(-np.sqrt(sigma1 * sigma2), np.sqrt(sigma1 * sigma2))
+        sigma = np.array([[sigma1, cov], [cov, sigma2]])
+
+        coef_gau = gaussian_coefficient(center, sigma, amplitude=amplitude)
+        gau_coef_list.append(coef_gau)
+
+    coef_gau = coefficient_wrapper(np.sum, *gau_coef_list)
+    coef_sum = coefficient_wrapper(np.sum, coef_radial, coef_dir, coef_gau)
+
+    return coef_sum
+
 def coef_strong():
     coef_radial = radial_coefficient(np.array([0, 0]), 1 / np.sqrt(200))
     coef_dir = directional_coefficient(np.array([1, 1]))
@@ -146,10 +170,15 @@ def coef_strong():
 def f_square(X, C, points):
     return (
         polynomial_function(C[0], 2)(X[:, 0], points)
-        # + C[0](points) * 10
         + 0
     )
 
+def f_square_const(X, C, points):
+    return (
+        polynomial_function(C[0], 2)(X[:, 0], points)
+        + C[0](points) * 10
+        + 0
+    )
 
 def f_sigmoid(X, C, points):
     return sigmoid_function(C[0])(X[:, 0], points) + 0
@@ -176,9 +205,10 @@ def square_strong_100():
     #     X,
     #     y,
     #     points,
-    #     [0.1, 0.2, 0.3],
-    #     [0.1, 0.2, 0.3],
-    #     [0.15, 0.3],
+    #     [0.1, 0.2, 0.3, 0.4, 0.5],
+    #     [0.1, 0.2, 0.3, 0.4, 0.5],
+    #     [0.1, 0.2, 0.3, 0.4, 0.5],
+    #     [0.1, 0.2, 0.3, 0.4, 0.5],
     #     100,
     #     "f_square",
     #     "coef_strong",
@@ -189,10 +219,10 @@ def square_strong_100():
         y,
         points,
         stacking_neighbour_count=0.3,
-        stacking_neighbour_leave_out_rate=0.3,
-        grf_neighbour_count=0.2,
+        stacking_neighbour_leave_out_rate=0.4,
+        grf_neighbour_count=0.3,
         grf_n_estimators=50,
-        gwr_neighbour_count=0.2,
+        gwr_neighbour_count=0.5,
         rf_n_estimators=2000,
     )
 
@@ -203,9 +233,9 @@ def square_strong_500():
     #     X,
     #     y,
     #     points,
-    #     [0.1, 0.2, 0.3],
-    #     [0.1, 0.2, 0.3],
-    #     [0.15, 0.3],
+    #     [0.1, 0.2, 0.3, 0.4, 0.5],
+    #     [0.1, 0.2, 0.3, 0.4, 0.5],
+    #     [0.1, 0.2, 0.3, 0.4, 0.5],
     #     500,
     #     "f_square",
     #     "coef_strong",
@@ -215,9 +245,9 @@ def square_strong_500():
         X,
         y,
         points,
-        stacking_neighbour_count=0.2,
-        stacking_neighbour_leave_out_rate=0.15,
-        grf_neighbour_count=0.2,
+        stacking_neighbour_count=0.3,
+        stacking_neighbour_leave_out_rate=0.1,
+        grf_neighbour_count=0.3,
         grf_n_estimators=50,
         gwr_neighbour_count=0.2,
         rf_n_estimators=2000,
@@ -228,36 +258,85 @@ def square_strong_5000():
     X, y, points = generate_sample(
         5000, f_square, coef_strong, random_seed=1, plot=True
     )
-    test_models(
-        X,
-        y,
-        points,
-        [0.02, 0.03, 0.04, 0.05],
-        [0.1, 0.2, 0.3],
-        [0.02, 0.03, 0.04, 0.05],
-        5000,
-        "f_square",
-        "coef_strong",
-    )
-
-    # fit_models(
+    # test_models(
     #     X,
     #     y,
     #     points,
-    #     stacking_neighbour_count=0.03,
-    #     stacking_neighbour_leave_out_rate=0.15,
-    #     grf_neighbour_count=0.03,
-    #     grf_n_estimators=50,
-    #     gwr_neighbour_count=0.03,
-    #     rf_n_estimators=2000,
+    #     [0.003, 0.005, 0.008, 0.01, 0.015, 0.02],
+    #     [0.1, 0.2, 0.3, 0.4, 0.5],
+    #     [0.003, 0.005, 0.008, 0.01, 0.015, 0.02],
+    #     5000,
+    #     "f_square",
+    #     "coef_strong",
     # )
 
+    fit_models(
+        X,
+        y,
+        points,
+        stacking_neighbour_count=0.015,
+        stacking_neighbour_leave_out_rate=0.4,
+        grf_neighbour_count=0.01,
+        grf_n_estimators=50,
+        gwr_neighbour_count=0.015,
+        rf_n_estimators=2000,
+    )
 
-def square_gau_strong():
-    # X, y, points = generate_sample(5000, f_square, coef_auto_gau_strong, random_seed=1, plot=True)
-    # X, y, points = generate_sample(5000, f_sigmoid, coef_strong, random_seed=1, plot=False)
-    pass
 
+def square_gau_strong_500():
+    X, y, points = generate_sample(500, f_square, coef_auto_gau_strong, random_seed=1, plot=True)
+    # test_models(
+    #     X,
+    #     y,
+    #     points,
+    #     [0.05, 0.08, 0.1, 0.15, 0.2],
+    #     [0.05, 0.1, 0.15, 0.2],
+    #     [0.05, 0.1, 0.2],
+    #     [0.05, 0.1, 0.2],
+    #     500,
+    #     "f_square",
+    #     "coef_gau_strong",
+    # )
+
+    fit_models(
+        X,
+        y,
+        points,
+        stacking_neighbour_count=0.08,
+        stacking_neighbour_leave_out_rate=0.1,
+        grf_neighbour_count=0.1,
+        grf_n_estimators=50,
+        gwr_neighbour_count=0.1,
+        rf_n_estimators=2000,
+    )
+
+
+def square_gau_weak_500():
+    X, y, points = generate_sample(500, f_square, coef_auto_gau_weak, random_seed=1, plot=True)
+    # test_models(
+    #     X,
+    #     y,
+    #     points,
+    #     [0.05, 0.08, 0.1, 0.15, 0.2],
+    #     [0.05, 0.1, 0.15, 0.2],
+    #     [0.05, 0.1, 0.2],
+    #     [0.05, 0.1, 0.2],
+    #     500,
+    #     "f_square",
+    #     "coef_gau_weak",
+    # )
+
+    fit_models(
+        X,
+        y,
+        points,
+        stacking_neighbour_count=0.08,
+        stacking_neighbour_leave_out_rate=0.15,
+        grf_neighbour_count=0.05,
+        grf_n_estimators=50,
+        gwr_neighbour_count=0.1,
+        rf_n_estimators=2000,
+    )
 
 def test_models(
     X,
@@ -266,6 +345,7 @@ def test_models(
     stacking_neighbour_count,
     stacking_neighbour_leave_out_rate,
     grf_neighbour_count,
+    gwr_neighbour_count,
     count,
     func,
     coef,
@@ -274,6 +354,7 @@ def test_models(
         X, y, points, stacking_neighbour_count, stacking_neighbour_leave_out_rate
     )
     grf_params = test_GRF(X, y, points, grf_neighbour_count)
+    gwr_params = test_GWR(X, y, points, gwr_neighbour_count)
 
     with open("simulation_params.jsonl", "a") as f:
         for params in stacking_params:
@@ -286,10 +367,22 @@ def test_models(
             params["func"] = func
             params["coef"] = coef
             f.write(json.dumps(params) + "\n")
+        for params in gwr_params:
+            params["count"] = count
+            params["func"] = func
+            params["coef"] = coef
+            f.write(json.dumps(params) + "\n")
 
     # Print the param with the best score
     print(max(stacking_params, key=lambda x: x["Stacking"]))
     print(max(grf_params, key=lambda x: x["GRF"]))
+    print(max(gwr_params, key=lambda x: x["GWR"]))
+
+    # Output the best result to jsonl
+    with open("simulation_param_best.jsonl", "a") as f:
+        f.write(json.dumps(max(stacking_params, key=lambda x: x["Stacking"])) + "\n")
+        f.write(json.dumps(max(grf_params, key=lambda x: x["GRF"])) + "\n")
+        f.write(json.dumps(max(gwr_params, key=lambda x: x["GWR"])) + "\n")
 
 
 def test_GRF(X, y, points, neighbour_counts):
@@ -300,50 +393,103 @@ def test_GRF(X, y, points, neighbour_counts):
 
     result = []
 
-    for neighbour_count in neighbour_counts:
-        model = WeightModel(
-            RandomForestRegressor(n_estimators=50),
-            distance_measure,
-            kernel_type,
-            neighbour_count=neighbour_count,
-        )
-        model.fit(X_plus, y, [points])
-        print("GRF:", model.llocv_score_, neighbour_count)
-        result.append({"GRF": model.llocv_score_, "neighbour_count": neighbour_count})
+    for use_x_plus in [True, False]:
+        for neighbour_count in neighbour_counts:
+            model = WeightModel(
+                RandomForestRegressor(n_estimators=50),
+                distance_measure,
+                kernel_type,
+                neighbour_count=neighbour_count,
+            )
+            if use_x_plus:
+                model.fit(X_plus, y, [points])
+            else:
+                model.fit(X_plus, y, [points])
+            print("GRF:", model.llocv_score_, neighbour_count, use_x_plus)
+            result.append(
+                {
+                    "GRF": model.llocv_score_,
+                    "neighbour_count": neighbour_count,
+                    "use_x_plus": use_x_plus,
+                }
+            )
 
     return result
 
 
 def test_stacking(X, y, points, neighbour_counts, leave_out_rates):
+    X_plus = np.concatenate([X, points], axis=1)
+
     distance_measure = "euclidean"
     kernel_type = "bisquare"
     local_estimator = DecisionTreeRegressor(splitter="random", max_depth=1)
 
     result = []
 
-    for neighbour_count in neighbour_counts:
-        for leave_out_rate in leave_out_rates:
-            model = StackingWeightModel(
-                local_estimator,
+    for use_x_plus in [True, False]:
+        for neighbour_count in neighbour_counts:
+            for leave_out_rate in leave_out_rates:
+                model = StackingWeightModel(
+                    local_estimator,
+                    distance_measure,
+                    kernel_type,
+                    neighbour_count=neighbour_count,
+                    neighbour_leave_out_rate=leave_out_rate,
+                )
+                if use_x_plus:
+                    model.fit(X_plus, y, [points])
+                else:
+                    model.fit(X, y, [points])
+                print(
+                    "Stacking:",
+                    model.llocv_score_,
+                    model.llocv_stacking_,
+                    "neighbour_count:",
+                    neighbour_count,
+                    "leave_out_rate:",
+                    leave_out_rate,
+                    "use_x_plus:",
+                    use_x_plus,
+                )
+                result.append(
+                    {
+                        "Stacking_Base": model.llocv_score_,
+                        "Stacking": model.llocv_stacking_,
+                        "neighbour_count": neighbour_count,
+                        "leave_out_rate": leave_out_rate,
+                        "use_x_plus": use_x_plus,
+                    }
+                )
+
+    return result
+
+
+def test_GWR(X, y, points, neighbour_counts):
+    X_plus = np.concatenate([X, points], axis=1)
+
+    distance_measure = "euclidean"
+    kernel_type = "bisquare"
+
+    result = []
+
+    for use_x_plus in [True, False]:
+        for neighbour_count in neighbour_counts:
+            model = WeightModel(
+                LinearRegression(),
                 distance_measure,
                 kernel_type,
                 neighbour_count=neighbour_count,
-                neighbour_leave_out_rate=leave_out_rate,
             )
-            model.fit(X, y, [points])
-            print(
-                "Stacking:",
-                model.llocv_score_,
-                model.llocv_stacking_,
-                neighbour_count,
-                leave_out_rate,
-            )
+            if use_x_plus:
+                model.fit(X_plus, y, [points])
+            else:
+                model.fit(X_plus, y, [points])
+            print("GWR:", model.llocv_score_, neighbour_count, use_x_plus)
             result.append(
                 {
-                    "Stacking_Base": model.llocv_score_,
-                    "Stacking": model.llocv_stacking_,
+                    "GWR": model.llocv_score_,
                     "neighbour_count": neighbour_count,
-                    "leave_out_rate": leave_out_rate,
+                    "use_x_plus": use_x_plus,
                 }
             )
 
@@ -353,4 +499,6 @@ def test_stacking(X, y, points, neighbour_counts, leave_out_rates):
 if __name__ == "__main__":
     # square_strong_100()
     # square_strong_500()
-    square_strong_5000()
+    # square_strong_5000()
+    square_gau_strong_500()
+    # square_gau_weak_500()
